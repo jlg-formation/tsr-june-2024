043f3a8adc9dcabe712e991739c2ae9a5560bd09
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-06-03 17:11:24 +0200

    dev: added lines

--- front/src/main.ts
+++ front/src/main.ts
@@ -7,6 +7,7 @@ const r = 45;
 const svgns = "http://www.w3.org/2000/svg";
 
 const samples = 10;
+const multiplicationFactor = 2;
 
 const container = document.querySelector("g.samples");
 if (container === null) {
@@ -24,4 +25,26 @@ for (let i = 0; i < samples; i++) {
   circle.setAttributeNS(null, "cy", String(y));
   circle.setAttributeNS(null, "r", (1).toString());
   container.appendChild(circle);
+}
+
+const lineContainer = document.querySelector("g.lines");
+if (lineContainer === null) {
+  throw new Error("oups");
+}
+
+for (let i = 0; i < samples; i++) {
+  const line = document.createElementNS(svgns, "line");
+
+  const angle1 = (i * (2 * Math.PI)) / samples;
+  const angle2 = multiplicationFactor * angle1;
+  const x1 = cx0 + r * Math.cos(angle1);
+  const y1 = cy0 + r * Math.sin(angle1);
+  const x2 = cx0 + r * Math.cos(angle2);
+  const y2 = cy0 + r * Math.sin(angle2);
+
+  line.setAttributeNS(null, "x1", x1 + "");
+  line.setAttributeNS(null, "y1", x2 + "");
+  line.setAttributeNS(null, "x2", y1 + "");
+  line.setAttributeNS(null, "y2", y2 + "");
+  lineContainer.appendChild(line);
 }
