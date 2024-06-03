87f4b39b99d8999c95f9aa65504ae37199dd4545
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-06-03 16:55:31 +0200

    dev: circle with points

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,3 +1,27 @@
 import "./style.css";
 
-console.log("coucou");
+const cx0 = 50;
+const cy0 = 50;
+const r = 45;
+
+const svgns = "http://www.w3.org/2000/svg";
+
+const samples = 10;
+
+const container = document.querySelector("g.samples");
+if (container === null) {
+  throw new Error("oups");
+}
+
+for (let i = 0; i < samples; i++) {
+  const circle = document.createElementNS(svgns, "circle");
+
+  const angle = (i * (2 * Math.PI)) / samples;
+  const x = cx0 + r * Math.cos(angle);
+  const y = cy0 + r * Math.sin(angle);
+
+  circle.setAttributeNS(null, "cx", x + "");
+  circle.setAttributeNS(null, "cy", String(y));
+  circle.setAttributeNS(null, "r", (1).toString());
+  container.appendChild(circle);
+}
